cecff6f675b42cae260639478e3a368a05fa20d7
Sebastian Behrens <sebastian.tschen.behrens@gmail.com>
2020-10-14 10:39:17 +0200

    add requirements

--- src/main.py
+++ src/main.py
@@ -3,7 +3,19 @@ from flask import Flask, request
 # creating the Flask application
 app = Flask(__name__)
 
+
 @app.route('/designs', methods=['POST'])
 def add_design():
     """upload a design and get the corresponding id to later download the stl file"""
-    request.get_json()
+    data = request.get_data()
+
+    print(data)
+
+    // Wonderbolts EneMeneMiste1Kiste 10000
+
+    return 121
+
+@app.route('/designs')
+def get_design():
+    """upload a design and get the corresponding id to later download the stl file"""
+    return "dfssdf"
